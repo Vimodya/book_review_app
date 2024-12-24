6ad9455d08012b7bcd0487484fe7fd6c682101ab
Liyanage C.V.K <130178817+Vimodya@users.noreply.github.com>
2024-12-24 01:21:24 +0000

    added

--- models/review.ts
+++ models/review.ts
@@ -7,6 +7,7 @@ const reviewSchema = new mongoose.Schema({
   author: { type: String, required: true },
   description: { type: String, required: true },
   rating: { type: Number, required: true, min: 1, max: 5 },
+  dateAdded: { type: Date, default: Date.now },
 });
 
 const Review = mongoose.models.Review || mongoose.model("Review", reviewSchema);
